8e4ba24f183e509217dbb4b2d16b94a2c78b30ae
enzo <enzostefani507@gmail.com>
2021-06-23 23:20:46 -0300

    se agrego subtitulo y archivo travis

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Ingeniería de software</h1>
+        <h2>La mejor materia</h2>
       </header>
     </div>
   );
